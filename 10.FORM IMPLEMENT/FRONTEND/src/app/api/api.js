c8bf902aaed84560762b8adcd1a9f9f83ef30928
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-09-11 19:08:14 +0530

    update api interceptor

--- 10.FORM IMPLEMENT/FRONTEND/src/app/api/api.js
+++ 10.FORM IMPLEMENT/FRONTEND/src/app/api/api.js
@@ -1,29 +1,32 @@
 import axios from 'axios'
 import { useAuth } from '../../context/AuthContext'
 
-const api = axios.create({
-    baseURL : 'http://localhost:5173/api',
-    withCredentials : true
-})
 
-export const useApi = ()=>{
-    const {accessToken}  = useAuth()
 
-   
-        api.interceptors.request.use(
-            (config)=>{
-                
-                if(accessToken){
-                    config.headers.Authorization = `Bearer ${accessToken}`
-                }
+export const useApi = () => {
 
-                return config
-            },
-            (error)=>{
-                return promise.reject(error)
-            }  
-        )
 
-        return api
-    
+    const api = axios.create({
+        baseURL: 'http://localhost:5173/api',
+        withCredentials: true
+    })
+    const { accessToken } = useAuth()
+
+
+    api.interceptors.request.use(
+        (config) => {
+
+            if (accessToken) {
+                config.headers.Authorization = `Bearer ${accessToken}`
+            }
+
+            return config
+        },
+        (error) => {
+            return promise.reject(error)
+        }
+    )
+
+    return api
+
 }
