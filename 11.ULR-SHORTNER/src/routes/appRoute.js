1b42d7111763a4d547f6353f6c5f643c8fb13fc0
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-09-15 17:04:25 +0530

    Add controller for all endpoints

--- 11.ULR-SHORTNER/src/routes/appRoute.js
+++ 11.ULR-SHORTNER/src/routes/appRoute.js
@@ -1,93 +1,14 @@
 import express from 'express'
 import { generateCode } from '../utils/generateCode.js'
 import urlModel from '../models/url.model.js'
+import { getAllUrlController, openUrlController, saveUrlController } from '../controller/url.controller.js'
 
 const router = express.Router()
 
-router.post('/url', async (req, res) => {
+router.post('/url', saveUrlController)
 
-    let { url } = req.body
+router.get("/open/:code", openUrlController)
 
-    if (!url) {
-        return res.status(400).json({
-            error: "Please enter a URL"
-        })
-    }
-
-    if (url.startsWith("http://") == false && url.startsWith("https://") == false) {
-        return res.status(400).json({
-            error: "Please enter a valid URL , starts with http:// or https://"
-        })
-    }
-
-    if (url.length > 2048) {
-        return res.status(400).json({
-            error: "URL is too long"
-        })
-    }
-
-    const code = generateCode()
-
-    const newUrl = await urlModel.create({
-
-        originalUrl: url,
-        shortCode: code
-
-    })
-
-    res.status(201).json({
-        message: "URL saved successfully..",
-        data: {
-            originalUrl: newUrl.originalUrl,
-            shortCode: newUrl.shortCode
-        }
-    })
-
-
-})
-
-router.get("/open/:code", async (req , res) => {
-
-    let {code}  = req.params
-
-    if(!code){
-        return res.status(400).json({
-            error : "URL is not valid..",
-            field : "Client, (not provided)"
-        })
-    }
-
-    let url = await urlModel.findOne({
-        shortCode : code
-    })
-
-    if(!url){
-        return res.status(400).json({
-            error : "URL not found..",
-            field :  "Database"
-        })
-    }
-
-    res.redirect(302, url.originalUrl)
-
-    await urlModel.findOneAndUpdate({shortCode : code}, {$inc : {clicks : 1}})
-})
-
-router.get("/all", async (req, res)=>{
-
-    let allURLs = await urlModel.find()
-
-    if(allURLs.length === 0){
-        return res.status(400).json({
-            message : "No URL Added..."
-        })
-    }
-    res.status(200).json({
-        message : "All url fetched..",
-        data : {
-        allURLs
-        }
-    })
-})
+router.get("/all", getAllUrlController)
 
 export default router
